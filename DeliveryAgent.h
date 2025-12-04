a98b67613ab03bf5eca69718f9b29b237270862b
Hasnat khan <sp24-bcs-039@isbstudent.comsats.edu.pk>
2025-12-04 23:55:19 +0500

    Add DeliveryAgent class with order assignment

---
+++ DeliveryAgent.h
@@ -0,0 +1,25 @@
+#ifndef DELIVERYAGENT_H
+#define DELIVERYAGENT_H
+
+#include <string>
+#include "Order.h"
+
+using namespace std;
+
+class DeliveryAgent {
+public:
+    string id;
+    string name;
+    string phone;
+    bool isAvailable;
+    Order currentOrder;
+
+    DeliveryAgent(string id, string name, string phone)
+        : id(id), name(name), phone(phone), isAvailable(true) {}
+
+    DeliveryAgent() : id(""), name(""), phone(""), isAvailable(true) {}
+
+    void assignOrder(Order order) {
+        currentOrder = order;
+        isAvailable = false;
+    }
